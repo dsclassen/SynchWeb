b4b08e1d3921aad15fabc3b0d756fe770a0eb98b
Neil A Smith <neil.smith@diamond.ac.uk>
2019-10-28 13:15:07 +0000

    Adding default secure flag for devServer

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -38,6 +38,7 @@ module.exports = (env, argv)  => {
             proxyReq.setHeader('Authorization', req.headers.authorization);
           }
         },
+        secure: false,
       },
     },
   },
